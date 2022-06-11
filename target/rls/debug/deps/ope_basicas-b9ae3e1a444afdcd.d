54b7964975d3dd3f97416caf7c974bf6f2eb0b1d
LuisJuarez@gmail.com <21al7207485@unedl.mx>
2022-06-10 21:46:34 -0500

    Preguntamos la edad de la persona

---
+++ target/rls/debug/deps/ope_basicas-b9ae3e1a444afdcd.d
@@ -0,0 +1,5 @@
+/home/juarez2112/rust_carp/ope_basicas/target/rls/debug/deps/ope_basicas-b9ae3e1a444afdcd.rmeta: src/main.rs
+
+/home/juarez2112/rust_carp/ope_basicas/target/rls/debug/deps/ope_basicas-b9ae3e1a444afdcd.d: src/main.rs
+
+src/main.rs:
---
+++ target/rls/debug/deps/ope_basicas-eaa0108a974e286c.d
@@ -0,0 +1,5 @@
+/home/juarez2112/rust_carp/ope_basicas/target/rls/debug/deps/ope_basicas-eaa0108a974e286c.rmeta: src/main.rs
+
+/home/juarez2112/rust_carp/ope_basicas/target/rls/debug/deps/ope_basicas-eaa0108a974e286c.d: src/main.rs
+
+src/main.rs:
